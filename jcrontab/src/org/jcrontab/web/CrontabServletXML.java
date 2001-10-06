8b5991eb2bbd3e60cc98b9a45259d61b34e0958e
iolalla <iolalla@aa82a720-bfcc-4a03-aa57-3b9ae0a7685a>
2001-10-06 21:48:51 +0000

    *** empty log message ***

--- jcrontab/src/org/jcrontab/web/CrontabServletXML.java
+++ jcrontab/src/org/jcrontab/web/CrontabServletXML.java
@@ -31,6 +31,12 @@ import javax.servlet.http.*;
 import org.jcrontab.CrontabEntryBean;
 import org.jcrontab.CrontabEntryDAO;
 import java.io.*;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.transform.stream.StreamResult;
+
 
 
 public class CrontabServletXML extends HttpServlet {
@@ -42,11 +48,50 @@ public class CrontabServletXML extends HttpServlet {
 
 	public void doGet (HttpServletRequest request,
     	       HttpServletResponse response) {
-		process(request, response);
+		show(request, response);
 	}
 
 	public void process(HttpServletRequest request,
-		HttpServletResponse response) { 
+		HttpServletResponse response) {
+                    
+                String Classname = request.getParameter("Classname").trim();
+                if (Classname.length() > 0) {
+                String Minutes = request.getParameter("Minutes").trim();
+                String Hours = request.getParameter("Hours").trim();
+                String Daysofmonth = request.getParameter("Daysofmonth").trim();
+                String Month = request.getParameter("Month").trim();
+                String Daysofweek = request.getParameter("Daysofweek").trim();
+                String Priority = request.getParameter("Priority").trim();
+                String Extrainfo = request.getParameter("Extrainfo").trim();              
+                StringBuffer sb = new StringBuffer();
+                sb.append(Minutes);
+                sb.append(" ");
+                sb.append(Hours);
+                sb.append(" ");
+                sb.append(Daysofmonth);
+                sb.append(" ");
+                sb.append(Month);
+                sb.append(" ");
+                sb.append(Daysofweek);
+                sb.append(" ");
+                sb.append(Classname);
+                sb.append(" ");
+                sb.append(Priority);
+                sb.append(" ");
+                sb.append(Extrainfo);
+                try {
+                CrontabEntryBean cb = new CrontabEntryBean(sb.toString());
+                CrontabEntryDAO.getInstance().store(cb);
+                show(request, response);
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+                } else {
+                show(request, response);   
+                }
+        }
+        public void show(HttpServletRequest request,
+		HttpServletResponse response) {
 
        		try {
 		PrintStream out = new PrintStream(response.getOutputStream()); 
@@ -60,7 +105,21 @@ public class CrontabServletXML extends HttpServlet {
 			sb.append(listOfBeans[i].toXML());
 		       }
 		        sb.append(printFooter());
-		        out.print(sb.toString());
+                                                
+                        TransformerFactory tFactory = 
+                            TransformerFactory.newInstance();
+
+                         
+                        Source xmlsource = new StreamSource(
+                            new StringReader((String)sb.toString()));
+                        
+                        Source xslsource = new StreamSource(
+                            new FileInputStream("view.xsl"));
+
+                        Transformer transformer = 
+                            tFactory.newTransformer(xslsource);
+                        transformer.transform(xmlsource, new StreamResult(out));
+		       // out.print(sb.toString());
 
 		out.close();
   		} catch (Exception ex) {
@@ -79,4 +138,5 @@ public class CrontabServletXML extends HttpServlet {
 	public static String printFooter() {
 		return "</page>";
 	}
+        
 }
