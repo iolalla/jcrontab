36599e734a17fe9bd7722aa0c362e35c1b855089
iolalla <iolalla@aa82a720-bfcc-4a03-aa57-3b9ae0a7685a>
2002-01-08 21:14:23 +0000

    *** empty log message ***

--- jcrontab/etc/create.sql
+++ jcrontab/etc/create.sql
@@ -11,17 +11,20 @@ month VARCHAR(64) DEFAULT '*',
 dayOfWeek VARCHAR(64) DEFAULT '*', 
 task VARCHAR(255),
 extraInfo VARCHAR(255),
-INDEX task(task(64))
 );
 # 
 # This is other option this is a binary format and what is 
 # stored in the blod is the CrontabentryBean. This only works 
 # in mysql
 #
-CREATE TABLE crontask(
-id integer,
-CrontabentryBean BLOB);
+#CREATE TABLE crontask(
+#id integer,
+#CrontabentryBean BLOB);
 #  This the same but for postgresql
 #CREATE TABLE crontask(
 #id integer,
 #CrontabentryBean OID);
+# This is the minimum insert to test the functionality some day will include
+# all the available tests
+# insert into events (task, extraInfo) values ('org.jcrontab.tests.TaskTest2', 
+# 'eachMinute1');
