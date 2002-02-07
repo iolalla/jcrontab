454a8c0af6a9690045b9125b7e9548db91265ff7
iolalla <iolalla@aa82a720-bfcc-4a03-aa57-3b9ae0a7685a>
2002-02-07 23:27:28 +0000

    little change in create.sql sorry :-)

--- trunk/jcrontab/etc/create.sql
+++ trunk/jcrontab/etc/create.sql
@@ -3,14 +3,14 @@
 # the size of the columns are orietnative and can be 
 # replaced with any other.
 #
-CREATE TABLE EVENTS ( 
-MINUTE VARCHAR(64) DEFAULT '*', 
-HOUR VARCHAR(64) DEFAULT '*',
-DAYOFMONTH VARCHAR(64) DEFAULT '*',
-MONTH VARCHAR(64) DEFAULT '*', 
-DAYOFWEEK VARCHAR(64) DEFAULT '*', 
-TASK VARCHAR(255),
-EXTRAINFO VARCHAR(255)
+CREATE TABLE events ( 
+minute VARCHAR(64) DEFAULT '*', 
+hour VARCHAR(64) DEFAULT '*',
+dayofmonth VARCHAR(64) DEFAULT '*',
+month VARCHAR(64) DEFAULT '*', 
+dayofweek VARCHAR(64) DEFAULT '*', 
+task VARCHAR(255),
+extrainfo VARCHAR(255)
 );
 # 
 # This is other option this is a binary format and what is 
@@ -18,12 +18,12 @@ EXTRAINFO VARCHAR(255)
 # in mysql
 #
 #CREATE TABLE crontask(
-#ID integer,
-#CRONTABENTRYBEAN BLOB);
+#id integer,
+#crontabentrybean BLOB);
 #  This the same but for postgresql
 #CREATE TABLE crontask(
-#ID integer,
-#CRONTABENTRYBEAN OID);
+#id integer,
+#crontabentrybean OID);
 # This is the minimum insert to test the functionality some day will include
 # all the available tests
 # insert into events (task, extraInfo) values ('org.jcrontab.tests.TaskTest2', 
