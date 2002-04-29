afc8d26f89af420ad2276f43c8639d3d3cf353bc
iolalla <iolalla@aa82a720-bfcc-4a03-aa57-3b9ae0a7685a>
2002-04-29 23:21:03 +0000

    added jedit plugin

---
+++ jcrontab/src/org/jcrontab/jedit/jcrontabOptionPane.java
@@ -0,0 +1,59 @@
+/**
+ *  This file is part of the jcrontab package
+ *  Copyright (C) 2001-2002 Israel Olalla
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA 02111-1307, USA
+ *
+ *  For questions, suggestions:
+ *
+ *  iolalla@yahoo.com
+ *
+ */
+ 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+import org.gjt.sp.jedit.*;
+
+public class jcrontabOptionPane extends AbstractOptionPane 
+{
+    private JTextField properties;
+    private JTextField frequency;
+
+    public jcrontabOptionPane() 
+	{
+        super("jcrontab");
+        setBorder(new EmptyBorder(5,5,5,5));
+
+        addComponent("Frequency", frequency = 
+				new JTextField(jEdit.getProperty("options.jcrontab.Frequency"), 3));
+        addComponent("Properties File", properties = 
+				new JTextField(jEdit.getProperty("options.jcrontab.Properties"), 15));
+				
+    }
+	
+    /**
+     * Called when the options dialog's `OK' button is pressed.
+     * This should save any properties saved in this option pane.
+     */
+    public void save() 
+	{            
+        jEdit.setProperty("options.jcrontab.Frequency", frequency.getText());            
+        jEdit.setProperty("options.jcrontab.Properties", properties.getText());            
+    }
+}
---
+++ jcrontab/src/org/jcrontab/jedit/jcrontabPlugin.java
@@ -0,0 +1,55 @@
+/**
+ *  This file is part of the jcrontab package
+ *  Copyright (C) 2001-2002 Israel Olalla
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA 02111-1307, USA
+ *
+ *  For questions, suggestions:
+ *
+ *  iolalla@yahoo.com
+ *
+ */
+ 
+import org.gjt.sp.jedit.*;
+import org.gjt.sp.jedit.gui.*;
+import org.gjt.sp.util.Log;
+import org.jcrontab.Crontab;
+
+public class jcrontabPlugin extends EditPlugin {
+	
+	public static final String NAME = "jcrontab";
+
+	//This variable defines the Crontab
+	static private Crontab crontab = null;
+   public void start() {
+	   	String events = jEdit.getProperty("options.jcrontab.Properties");
+		int iFrec = Integer.parseInt(
+						jEdit.getProperty("options.jcrontab.Frequency"));
+			crontab = Crontab.getInstance();
+			try {
+				crontab.init(events,iFrec);
+				Log.log(Log.MESSAGE, jcrontabPlugin.class, 
+						"Jcrontab is Working... ok");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+   }
+
+    public void createOptionPanes(OptionsDialog optionsDialog) {
+
+			optionsDialog.addOptionPane(new jcrontabOptionPane());
+    }
+}
