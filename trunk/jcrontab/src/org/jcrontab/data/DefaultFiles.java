f475dd375fb79cfcedc648560c9ea0bd9ac98751
iolalla <iolalla@aa82a720-bfcc-4a03-aa57-3b9ae0a7685a>
2002-05-11 17:43:49 +0000

    continue working for 1.0

--- trunk/jcrontab/src/org/jcrontab/data/DefaultFiles.java
+++ trunk/jcrontab/src/org/jcrontab/data/DefaultFiles.java
@@ -24,10 +24,84 @@
  */
  
 package org.jcrontab.data;
- 
+
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+/**
+ *	This class is a utility to make easier the instalation and use of jcrontab
+ *	What it does is to create the default files "jcrontab.properties" and 
+ * "crontab" and set a default initial properties ready to work in any system
+ *  The reason why this class was added was to make it easier to integrate with
+ *  jEdit
+ * @author $Author: iolalla $
+ * @version $Revision: 1.2 $
+ */
 public class DefaultFiles {
 	
-	private String jcrontabDir = "jcrontab";
-	private String propertiesFile = "jcrontab.properties";
-	private String eventsFile = "crontab";
+	private static String home;
+	private static String FileSeparator;
+	private static String jcrontabDir = "jcrontab";
+	private static String propertiesFile = "jcrontab.properties";
+	private static String crontabFile = "crontab";
+	private static String dir = home + FileSeparator + jcrontabDir;
+	static { 
+		home = System.getProperty("user.home");
+		FileSeparator = System.getProperty("file.separator");
+	}
+	
+	/**
+	 *	This method loads creates the default Properties file in order to have
+	 *  jcrontab working. The other option was to use getResourceAsStream with 
+	 *  the properties but with this solution to change the properties was more 
+	 *  complicated
+	 *	@throws Exception
+	 */
+	public static void createPorpertiesFile() throws Exception {
+		File propFile = new File(dir + propertiesFile);
+		propFile.createNewFile();
+		Class cla = FileSource.class;
+        BufferedReader input = new BufferedReader(
+            new InputStreamReader(cla.getResourceAsStream(propertiesFile)));
+		BufferedWriter output = new BufferedWriter(new FileWriter(propFile));
+			String strLine;
+				
+			while((strLine = input.readLine()) != null){
+				//System.out.println(strLine);
+				strLine = strLine.trim();
+				if (strLine.indexOf("{$HOME}") != -1) {
+					strLine = "org.jcrontab.data.file = "+ 
+							   home + 
+							   FileSeparator  + 
+							   ".jcrontab/crontab";
+				}
+				output.write(strLine);
+			}
+			input.close();
+			output.close();
+	}
+
+	/**
+	 *	This method creates the default crontabFile This file is void to 
+	 * Avoid launching undesired tasks.
+	 *
+	 */
+	public static void createCrontabFile() throws Exception {
+		File evFile = new File(dir + crontabFile);
+		evFile.createNewFile();
+		
+	}
+	/**
+	 *	This method creates the default jcrontabDir 
+	 *
+	 */
+	private static void createJcrontabDir() {
+		File distDir = new File(dir);
+		distDir.mkdir();
+	}
 }
