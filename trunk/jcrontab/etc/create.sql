aac25931a39e6cfbd781b7d6cafbdb49edd195f0
iolalla <iolalla@aa82a720-bfcc-4a03-aa57-3b9ae0a7685a>
2002-08-30 18:42:09 +0000

    sorry :-)

--- trunk/jcrontab/etc/create.sql
+++ trunk/jcrontab/etc/create.sql
@@ -3,15 +3,19 @@
 # the size of the columns are orietnative and can be 
 # replaced with any other.
 #
-CREATE TABLE events ( 
+CREATE TABLE events (
+second VARCHAR(64) DEFAULT '0',
 minute VARCHAR(64) DEFAULT '*', 
 hour VARCHAR(64) DEFAULT '*',
 dayofmonth VARCHAR(64) DEFAULT '*',
 month VARCHAR(64) DEFAULT '*', 
-dayofweek VARCHAR(64) DEFAULT '*', 
+dayofweek VARCHAR(64) DEFAULT '*',
+year VARCHAR(64) DEFAULT '*',
 task VARCHAR(255),
 extrainfo VARCHAR(255)
 );
+# This update sets correctly the database if you have and older version of Jcrontab
+# ALTER TABLE events ADD second VARCHAR(64) DEFAULT '0', year VARCHAR(64) DEFAULT '*';
 # 
 # This is other option this is a binary format and what is 
 # stored in the blod is the CrontabentryBean. This only works 
@@ -26,5 +30,6 @@ extrainfo VARCHAR(255)
 #crontabentrybean OID);
 # This is the minimum insert to test the functionality some day will include
 # all the available tests
-# insert into events (task, extrainfo) values ('org.jcrontab.tests.TaskTest2', 
-# 'eachMinute1');
+# insert into events (second, minute, task, extrainfo) values ('6', '*/2', 'org.jcrontab.tests.TaskTest2', 'each two minutes in second 6')
+# insert into events (second, minute, task, extrainfo) values ('15', '*/5', 'org.jcrontab.tests.TaskTest2', 'each five minutes in second 15');
+# select * from events
