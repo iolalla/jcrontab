7ee39b15f6be1efaeabebf2f5f81be783b993d3d
kingjr3 <kingjr3@aa82a720-bfcc-4a03-aa57-3b9ae0a7685a>
2006-01-29 02:52:36 +0000

    Modified createCrontabStream() to check to see if crontab file is in the classpath first, then it will look into the filesystem.  This is handy when using jcrontab in a web based implementation.

--- jcrontab/src/org/jcrontab/data/FileSource.java
+++ jcrontab/src/org/jcrontab/data/FileSource.java
@@ -39,230 +39,253 @@ import java.util.Vector;
 import org.jcrontab.Crontab;
 
 /**
- * This class Is the implementation of DataSource to access 
- * Info in a FileSystem
- * @author $Author: iolalla $
- * @version $Revision: 1.48 $
+ * This class Is the implementation of DataSource to access Info in a FileSystem
+ * 
+ * @author $Author: kingjr3 $
+ * @version $Revision: 1.49 $
  */
 public class FileSource implements DataSource {
-	
-	private CrontabParser cp = new CrontabParser();
 
-	private static FileSource instance;
-    
-	private CrontabEntryBean[] cachedBeans = null;
-	
-	protected long lastModified;
-	
-	private String crontab_file = "crontab";
-    
-    /** 
-	* Creates new FileSource 
-	*/
-	
+    private CrontabParser cp = new CrontabParser();
+
+    private static FileSource instance;
+
+    private CrontabEntryBean[] cachedBeans = null;
+
+    protected long lastModified;
+
+    private String crontab_file = "crontab";
+
+    /**
+     * Creates new FileSource
+     */
+
     protected FileSource() {
-			if (Crontab.getInstance().getProperty(
-								"org.jcrontab.data.file") == null) 
-				Crontab.getInstance().setProperty(
-								"org.jcrontab.data.file", crontab_file);
-    }	
+        if (Crontab.getInstance().getProperty("org.jcrontab.data.file") == null)
+            Crontab.getInstance().setProperty("org.jcrontab.data.file", crontab_file);
+    }
+
     /**
-	 *	This method returns the singleton is very important to grant
-	 *  That only a Thread accesses at a time
-	 */
+     * This method returns the singleton is very important to grant That only a
+     * Thread accesses at a time
+     */
     public synchronized DataSource getInstance() {
-		if (instance == null) {
-		instance = new FileSource();
-		}
-		return instance;
+        if (instance == null) {
+            instance = new FileSource();
+        }
+        return instance;
     }
-	
+
     /**
-	 *	This method searches the given Bean  from the File
-	 *  @return CrontabEntryBean beans Array the result of the search
-	 *  @param CrontabEntryBean the CrontabEntryBean you want to search
-	 *  @throws CrontabEntryException when it can't parse the line correctly
-	 *  @throws IOException If it can't access correctly to the File
-	 *  @throws DataNotFoundException whe it can't find nothing in the file 
-	 */
-    public synchronized CrontabEntryBean find(CrontabEntryBean ceb) 
-    	throws CrontabEntryException, IOException, DataNotFoundException {
+     * This method searches the given Bean from the File
+     * 
+     * @return CrontabEntryBean beans Array the result of the search
+     * @param CrontabEntryBean
+     *            the CrontabEntryBean you want to search
+     * @throws CrontabEntryException
+     *             when it can't parse the line correctly
+     * @throws IOException
+     *             If it can't access correctly to the File
+     * @throws DataNotFoundException
+     *             whe it can't find nothing in the file
+     */
+    public synchronized CrontabEntryBean find(CrontabEntryBean ceb) throws CrontabEntryException, IOException,
+            DataNotFoundException {
         CrontabEntryBean[] cebra = findAll();
-		for (int i = 0; i < cebra.length ; i++) {
-			if (cebra[i].equals(ceb)) {
-//System.out.println("cebra encontrada : " + cebra[i]);
-				return cebra[i];
-			}
-		}
-		throw new DataNotFoundException("Unable to find :" + ceb);
+        for (int i = 0; i < cebra.length; i++) {
+            if (cebra[i].equals(ceb)) {
+                // System.out.println("cebra encontrada : " + cebra[i]);
+                return cebra[i];
+            }
+        }
+        throw new DataNotFoundException("Unable to find :" + ceb);
     }
 
-	protected InputStream createCrontabStream(String name)
-		throws IOException {
-		return new FileInputStream(name);
-	}
+    protected InputStream createCrontabStream(String name) throws IOException {
 
-   	/**
-	 *	This method searches all the CrontabEntryBean from the File
-	 *  @return CrontabEntryBean beans Array the result of the search
-	 *  @throws CrontabEntryException when it can't parse the line correctly
-	 *  @throws IOException If it can't access correctly to the File
-	 *  @throws DataNotFoundException whe it can't find nothing in the file 
-	 */
-    public synchronized CrontabEntryBean[] findAll() throws CrontabEntryException, 
-			IOException, DataNotFoundException {
-                
-            boolean[] bSeconds = new boolean[60];
-            boolean[] bYears = new boolean[10];
-            
-	        Vector listOfLines = new Vector();
-            Vector listOfBeans = new Vector();
-            // Class cla = FileSource.class;
-            // BufferedReader input = new BufferedReader(new FileReader(strFileName));
-            // This Line allows the crontab to be included in a jar file
-            // and accessed from anywhere
-            String filename = Crontab.getInstance().getProperty("org.jcrontab.data.file");
-            
-				// open the file
-				final InputStream fis = createCrontabStream(filename);
-				BufferedReader input = new BufferedReader(
-												new InputStreamReader(fis));
-				
-				String strLine;
-				
-				while((strLine = input.readLine()) != null){
-					//System.out.println(strLine);
-					strLine = strLine.trim();
-					listOfLines.add(strLine);
-				}
-				input.close();
-					if (listOfLines.size() > 0) {
-					for (int i = 0; i < listOfLines.size() ; i++) {
-						String strLines = (String)listOfLines.get(i);
-						// Skips blank lines and comments
-						if(strLines.equals("") || strLines.charAt(0) == '#'){
-						} else {
-						//System.out.println(strLines);
+        // Try the classpath first
+        InputStream input = FileSource.class.getClassLoader().getResourceAsStream(name);
+        if (input == null)
+            input = new FileInputStream(name);
+        return input;
 
-						CrontabEntryBean entry = cp.marshall(strLines);
-						listOfBeans.add(entry);
-						}
-					}
-					} else {
-					throw new DataNotFoundException("No CrontabEntries available");
-					}
-           
-            			int sizeOfBeans = listOfBeans.size();
-				if ( sizeOfBeans == 0 ){
-					throw new DataNotFoundException("No CrontabEntries available");
-				} else {
-					CrontabEntryBean[] finalBeans = 
-						new CrontabEntryBean[sizeOfBeans];
-					for (int i = 0; i < sizeOfBeans; i++) {
-						//Added to have different Beans identified
-						finalBeans[i] = (CrontabEntryBean)listOfBeans.get(i);
-						finalBeans[i].setId(i);
-					}
-					cachedBeans = finalBeans;
-				}
-				return cachedBeans;
-    	}
-		
+    }
+
     /**
-	 *	This method removes the CrontabEntryBean array from the File
-	 *  @param CrontabEntryBean bean teh array of beans to remove
-	 *  @throws Exception 
-	 */
-	 
-    public synchronized void remove(CrontabEntryBean[] ceb) throws Exception {
-	
-        CrontabEntryBean[] thelist = findAll();
-	    CrontabEntryBean[] result = new CrontabEntryBean[thelist.length - ceb.length];
-	    
-	    for (int i = 0; i < thelist.length ; i++) {
-		    if (thelist[i] != null ) thelist[i].setId(-1);
-		    for (int y = 0; y < ceb.length ; y++) {
-		    	   ceb[y].setId(-1);
-			    if (thelist[i] != null && thelist[i].equals(ceb[y])) {
-				    thelist[i] = null;
-			    } 
-		    }
-	    }
-	    
-	    int resultCounter = 0;
-	    for (int i = 0; i < thelist.length ; i++) {
-		    if(thelist[i] != null) {
-			result[resultCounter] = thelist[i];
-			resultCounter++;
-	        }
-	    }
-            storeAll(result);
-	}
-    
-	/**
-	 *	This method saves the CrontabEntryBean array the actual problem with this
-	 *  method is that doesn�t store comments and blank lines from the original
-	 *  file any ideas?
-	 *  @param CrontabEntryBean bean this method stores the array of beans
-	 *  @throws CrontabEntryException when it can't parse the line correctly
-	 *  @throws IOException If it can't access correctly to the File
-	 *  @throws DataNotFoundException whe it can't find nothing in the file usually 
-	 *  Exception should'nt this 
-	 */
-    public synchronized void storeAll(CrontabEntryBean[] list) throws 
-               CrontabEntryException, FileNotFoundException, IOException {
+     * This method searches all the CrontabEntryBean from the File
+     * 
+     * @return CrontabEntryBean beans Array the result of the search
+     * @throws CrontabEntryException
+     *             when it can't parse the line correctly
+     * @throws IOException
+     *             If it can't access correctly to the File
+     * @throws DataNotFoundException
+     *             whe it can't find nothing in the file
+     */
+    public synchronized CrontabEntryBean[] findAll() throws CrontabEntryException, IOException, DataNotFoundException {
 
-		    File fl = new File(Crontab.getInstance()
-								.getProperty("org.jcrontab.data.file"));
-		    PrintStream out = new PrintStream(new FileOutputStream(fl));
-            for (int i = 0; i < list.length; i++){
-			if (list[i] != null) {
-		    			out.println("#");
-                			out.println(cp.unmarshall(list[i]));
-			}
+        boolean[] bSeconds = new boolean[60];
+        boolean[] bYears = new boolean[10];
+
+        Vector listOfLines = new Vector();
+        Vector listOfBeans = new Vector();
+        // Class cla = FileSource.class;
+        // BufferedReader input = new BufferedReader(new
+        // FileReader(strFileName));
+        // This Line allows the crontab to be included in a jar file
+        // and accessed from anywhere
+        String filename = Crontab.getInstance().getProperty("org.jcrontab.data.file");
+
+        // open the file
+        final InputStream fis = createCrontabStream(filename);
+        BufferedReader input = new BufferedReader(new InputStreamReader(fis));
+
+        String strLine;
+
+        while ((strLine = input.readLine()) != null) {
+            // System.out.println(strLine);
+            strLine = strLine.trim();
+            listOfLines.add(strLine);
+        }
+        input.close();
+        if (listOfLines.size() > 0) {
+            for (int i = 0; i < listOfLines.size(); i++) {
+                String strLines = (String) listOfLines.get(i);
+                // Skips blank lines and comments
+                if (strLines.equals("") || strLines.charAt(0) == '#') {
+                } else {
+                    // System.out.println(strLines);
+
+                    CrontabEntryBean entry = cp.marshall(strLines);
+                    listOfBeans.add(entry);
+                }
             }
-	    out.println("#");
-	}
-	/**
-	 *  This method saves the CrontabEntryBean array the actual problem with this
-	 *  method is that doesn�t store comments and blank lines from the original
-	 *  file any ideas?
-	 *  @param CrontabEntryBean bean this method stores the array of beans
-	 *  @throws CrontabEntryException when it can't parse the line correctly
-	 *  @throws IOException If it can't access correctly to the File
-	 *  @throws DataNotFoundException whe it can't find nothing in the file usually 
-	 *  Exception should'nt this 
-	 */
-	public synchronized void store(CrontabEntryBean[] beans) throws CrontabEntryException, 
-			IOException, DataNotFoundException {
-            CrontabEntryBean[]  thelist = null;
-	    boolean succedded = false;
-	    try {
+        } else {
+            throw new DataNotFoundException("No CrontabEntries available");
+        }
+
+        int sizeOfBeans = listOfBeans.size();
+        if (sizeOfBeans == 0) {
+            throw new DataNotFoundException("No CrontabEntries available");
+        } else {
+            CrontabEntryBean[] finalBeans = new CrontabEntryBean[sizeOfBeans];
+            for (int i = 0; i < sizeOfBeans; i++) {
+                // Added to have different Beans identified
+                finalBeans[i] = (CrontabEntryBean) listOfBeans.get(i);
+                finalBeans[i].setId(i);
+            }
+            cachedBeans = finalBeans;
+        }
+        return cachedBeans;
+    }
+
+    /**
+     * This method removes the CrontabEntryBean array from the File
+     * 
+     * @param CrontabEntryBean
+     *            bean teh array of beans to remove
+     * @throws Exception
+     */
+
+    public synchronized void remove(CrontabEntryBean[] ceb) throws Exception {
+
+        CrontabEntryBean[] thelist = findAll();
+        CrontabEntryBean[] result = new CrontabEntryBean[thelist.length - ceb.length];
+
+        for (int i = 0; i < thelist.length; i++) {
+            if (thelist[i] != null)
+                thelist[i].setId(-1);
+            for (int y = 0; y < ceb.length; y++) {
+                ceb[y].setId(-1);
+                if (thelist[i] != null && thelist[i].equals(ceb[y])) {
+                    thelist[i] = null;
+                }
+            }
+        }
+
+        int resultCounter = 0;
+        for (int i = 0; i < thelist.length; i++) {
+            if (thelist[i] != null) {
+                result[resultCounter] = thelist[i];
+                resultCounter++;
+            }
+        }
+        storeAll(result);
+    }
+
+    /**
+     * This method saves the CrontabEntryBean array the actual problem with this
+     * method is that doesn�t store comments and blank lines from the original
+     * file any ideas?
+     * 
+     * @param CrontabEntryBean
+     *            bean this method stores the array of beans
+     * @throws CrontabEntryException
+     *             when it can't parse the line correctly
+     * @throws IOException
+     *             If it can't access correctly to the File
+     * @throws DataNotFoundException
+     *             whe it can't find nothing in the file usually Exception
+     *             should'nt this
+     */
+    public synchronized void storeAll(CrontabEntryBean[] list) throws CrontabEntryException, FileNotFoundException,
+            IOException {
+
+        File fl = new File(Crontab.getInstance().getProperty("org.jcrontab.data.file"));
+        PrintStream out = new PrintStream(new FileOutputStream(fl));
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] != null) {
+                out.println("#");
+                out.println(cp.unmarshall(list[i]));
+            }
+        }
+        out.println("#");
+    }
+
+    /**
+     * This method saves the CrontabEntryBean array the actual problem with this
+     * method is that doesn�t store comments and blank lines from the original
+     * file any ideas?
+     * 
+     * @param CrontabEntryBean
+     *            bean this method stores the array of beans
+     * @throws CrontabEntryException
+     *             when it can't parse the line correctly
+     * @throws IOException
+     *             If it can't access correctly to the File
+     * @throws DataNotFoundException
+     *             whe it can't find nothing in the file usually Exception
+     *             should'nt this
+     */
+    public synchronized void store(CrontabEntryBean[] beans) throws CrontabEntryException, IOException,
+            DataNotFoundException {
+        CrontabEntryBean[] thelist = null;
+        boolean succedded = false;
+        try {
             thelist = findAll();
-	    succedded = true;
-	    } catch (Exception e) {
-		    if (e instanceof DataNotFoundException) {
-			    storeAll(beans);
-		    } else {
-		    throw new 
-		    	DataNotFoundException("Unable to find CrontabEntries");
-		    }
-	    }
-	    if (succedded) {
-		    int size = (thelist.length +1 );
-		    
-		    CrontabEntryBean[] resultlist = new CrontabEntryBean[size];
-		    Vector ve = new Vector();
-		    for (int i = 0; i < thelist.length; i++){
-			ve.add(thelist[i]);
-		    }
-				for (int i = 0; i < beans.length; i++) {
-					ve.add(beans[i]);
-				}
-		    for (int i = 0; i < ve.size(); i++){
-			resultlist[i] = (CrontabEntryBean)ve.get(i);
-		    }
-		    storeAll(resultlist);
-	    }
-	}
+            succedded = true;
+        } catch (Exception e) {
+            if (e instanceof DataNotFoundException) {
+                storeAll(beans);
+            } else {
+                throw new DataNotFoundException("Unable to find CrontabEntries");
+            }
+        }
+        if (succedded) {
+            int size = (thelist.length + 1);
+
+            CrontabEntryBean[] resultlist = new CrontabEntryBean[size];
+            Vector ve = new Vector();
+            for (int i = 0; i < thelist.length; i++) {
+                ve.add(thelist[i]);
+            }
+            for (int i = 0; i < beans.length; i++) {
+                ve.add(beans[i]);
+            }
+            for (int i = 0; i < ve.size(); i++) {
+                resultlist[i] = (CrontabEntryBean) ve.get(i);
+            }
+            storeAll(resultlist);
+        }
+    }
 }
